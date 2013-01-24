eff653dfea1fe8d293a1b862ebaefa4265f8e0a7
harbutpe <harbutpe@gmail.com>
2013-01-24 08:55:36 +0100

    teras s popisom a do 20

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-var max = 10;
+//tereaz je to do 20
+var max = 20;
 
 for ( i = 0; i <= max; i++){
   var b = Math.pow(2, i);
-  WScript.echo( b );
+  WScript.echo( '2^' + i + '=' + b ); //aj s pokecom
 }
