5f86eac8e54f55e277457a2caeff6f0694ffa5fd
harbutpe <harbutpe@gmail.com>
2013-01-24 09:18:55 +0100

    anotder great version

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
 //tereaz je to do 20
+
+//metoda repeat vrati poc opakovani retazca
+if ( !String.prototype.repeat ) String.prototype.repeat = function(poc) {
+	var pom = '';
+	for (var i = 0; i < poc; i++){
+		pom += this.toString();
+	}
+	return pom;
+}
+
 var max = 20;
 
+WScript.echo('-'.repeat(20));
 for ( i = 0; i <= max; i++){
   var b = Math.pow(2, i);
   WScript.echo( '2^' + i + '=' + b ); //aj s pokecom
 }
+WScript.echo('-'.repeat(20));
+
